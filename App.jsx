45ef7f98eb1e6ef09efe95ffb297491008833d45
Vibha Chandola <93372460+vibha-cloud@users.noreply.github.com>
2023-09-24 18:16:42 +0530

    initial page

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import Faq from "./pages/Faq";
+import Guide from "./pages/Guide";
+
+const App = () => {
+  return (
+    <div>
+      {/* <Home /> */}
+      <Faq />
+      {/* <Guide /> */}
+    </div>
+  );
+};
+
+export default App;
