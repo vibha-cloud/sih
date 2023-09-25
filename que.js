b2aa63ca390ee724e066444a74b9c230e58085fa
Vibha Chandola <93372460+vibha-cloud@users.noreply.github.com>
2023-09-25 07:51:52 +0530

    que for faq

---
+++ que.js
@@ -0,0 +1,62 @@
+export const faqs = [
+  {
+    id: 1,
+    question: "1. What is e-waste?",
+    answer:
+      "E-waste, or electronic waste, refers to discarded electrical or electronic devices. This can include computers, smartphones, televisions, printers, and other electronics.",
+  },
+  {
+    id: 2,
+    question: "2. Why is e-waste management important?",
+    answer:
+      "E-waste contains harmful materials that can negatively impact the environment and human health if not managed properly. Recycling and proper disposal help recover valuable resources and reduce pollution.",
+  },
+  {
+    id: 3,
+    question: "3. How can I dispose of e-waste responsibly?",
+    answer:
+      "Look for authorized e-waste disposal facilities, recycling centers, or collection events in your area. Ensure your e-waste is handled by certified recyclers to guarantee responsible disposal.",
+  },
+  {
+    id: 4,
+    question: "4. What should I do before disposing of my electronic device?",
+    answer:
+      "Back up and securely delete any personal data from your device. Remove batteries, SIM cards, or memory cards, and follow the manufacturer's instructions for safe disposal",
+  },
+  {
+    id: 5,
+    question: "5. Can I recycle old or broken electronics?",
+    answer:
+      "Yes, many electronics can be recycled, including broken or non-functioning devices. Recycling facilities can recover valuable materials from these electronics.",
+  },
+  {
+    id: 6,
+    question: "6. What happens to electronics once they are recycled?",
+    answer:
+      "Recycled electronics are dismantled, and components are sorted for recovery of valuable materials like metals, plastics, and precious metals. These materials are then used to produce new products.",
+  },
+  {
+    id: 7,
+    question: "7. What materials can be recovered from recycled electronics?",
+    answer:
+      "Common materials recovered include metals (copper, aluminum, gold), plastics, glass, and various other materials used in electronics manufacturing.",
+  },
+  {
+    id: 8,
+    question: "8. Are there any regulations regarding e-waste disposal?",
+    answer:
+      "Yes, many regions have specific laws and regulations governing the proper disposal and recycling of e-waste. Check with local authorities to understand the regulations in your area.",
+  },
+  {
+    id: 9,
+    question: "9. Is there a cost associated with e-waste recycling?",
+    answer:
+      "Recycling fees may vary depending on the type and quantity of e-waste. Some programs offer free recycling, while others may charge a small fee.",
+  },
+  {
+    id: 10,
+    question: "10. What are the environmental benefits of recycling e-waste?",
+    answer:
+      "Recycling e-waste reduces the demand for raw materials, energy consumption, greenhouse gas emissions, and the amount of waste sent to landfills, ultimately preserving natural resources and the environment.",
+  },
+];
